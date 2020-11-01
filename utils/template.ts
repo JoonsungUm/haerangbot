a969794f2b7da0735e7643ba731f27ae8343b5f2
joonsung.um <joonsung.um@gmail.com>
2020-11-02 03:35:10 +0900

    fix dueDate

--- utils/template.ts
+++ utils/template.ts
@@ -1,6 +1,16 @@
 import { PostAttributes } from '../db/models/Post'
 import { format } from 'date-fns'
 
+const getDueDate = (startDate, endDate) => {
+  console.log(startDate)
+  console.log(endDate)
+  if (startDate === '0000-00-00' || endDate === '0000-00-00') {
+    return '미등록'
+  } else {
+    return `${startDate}~${endDate}`
+  }
+}
+
 export const getCardItems = async (
   posts: PostAttributes[],
 ) => {
@@ -9,7 +19,7 @@ export const getCardItems = async (
 
     return {
       title,
-      description: `-등록일: ${format(createdAt, 'yyyy-MM-dd')}\n-게재기간: ${startDate}~${endDate}\n-담당부서: ${department}\n-담당자: ${managerName}/${managerPhone}`,
+      description: `-등록일: ${format(createdAt, 'yyyy-MM-dd')}\n-게재기간: ${getDueDate(startDate, endDate)}\n-담당부서: ${department}\n-담당자: ${managerName}/${managerPhone}`,
       buttons: [
         {
           action: 'webLink',
